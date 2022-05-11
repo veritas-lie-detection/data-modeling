82e4445cf254184b265a67b3c2cf31242b874ac3
forsaaan <ekzhou@calpoly.edu>
2022-05-11 11:48:28 -0700

    Update old_modeling_script.py

--- old_modeling_script.py
+++ old_modeling_script.py
@@ -1,21 +1,9 @@
-import time
-import spacy
-from tracemalloc import stop
-import pandas as pd
-import nltk
 from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
-t0 = time.time()
-
-from collections import Counter
-import pandas as pd
 import nltk
-from nltk.util import ngrams
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn import feature_extraction, linear_model, model_selection, preprocessing
-from sklearn.metrics import accuracy_score
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -26,30 +14,12 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, plot_confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import spacy
 
 
-stop_words = stopwords.words('english')
-
-df = pd.read_csv("data-collection/df.csv")
-
-
-df = pd.melt(df, id_vars=['url', 'classification'], value_vars=[
-        '1A', "7", "7A"])
-
-df = df[df['value'] != "undefined"]
-
-
-nlp = spacy.load('en_core_web_sm')
-df['value'] = df['value'].apply(lambda x: " ".join(
-    [ent.text for ent in nlp(x) if not ent.ent_type_]))
-# print(df[df['value'] != "undefined"])
-# print(len(df))
-# print(len(df[df['value'] != "undefined"]))
-
-def tokenizeandstopwords(text):
+def tokenize_and_stopwords(text):
     tokens = nltk.word_tokenize(text)
     # taken only words (not punctuation)
     token_words = [w for w in tokens if w.isalpha() or w.isnumeric()]
@@ -76,142 +46,98 @@ def benfords(text):
     return leading_digits
 
 
-df['value'] = df['value'].apply(tokenizeandstopwords)
+def test_model(pipeline, train_x, train_y, test_x, test_y):
+    model = pipeline.fit(train_x, train_y)
+    prediction = model.predict(test_x)
+    print("accuracy: {}%".format(round(accuracy_score(test_y, prediction) * 100, 2)))
+    print(confusion_matrix(test_y, prediction))
+    print(classification_report(test_y, prediction, digits=4))
 
 
+def split_by_doc(df, test_size, target_name, doc_index="url", seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    ids = df[doc_index].unique()
+    train = np.random.choice(ids, size=int(ids.size * (1 - test_size)), replace=False)
 
-# df = pd.read_csv("df_tokenized.csv")
+    train_df = df[df[doc_index].isin(train)]
+    test_df = df.loc[~df.index.isin(train_df.index)]
 
-df['test'] = df['value'].apply(benfords)
-df2 = pd.json_normalize(df['test'])
-df = pd.concat([df.drop(['test'], axis=1), df2], axis=1)
-
-
-df.dropna(inplace=True)
-sia = SentimentIntensityAnalyzer()
-
-
-df['test'] = df['value'].apply(lambda x: sia.polarity_scores(x))
-df2 = pd.json_normalize(df['test'])
-df = pd.concat([df.drop(['test'], axis=1), df2], axis=1)
-df.to_csv('df_final_without_ent.csv')
+    return (train_df.drop([target_name], axis=1), train_df[target_name],
+            test_df.drop([target_name], axis=1), test_df[target_name])
 
 
+if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_md")
+    stop_words = stopwords.words("english")
 
-# df = pd.read_csv("df_final.csv")
-df.dropna(inplace=True)
+    df = pd.read_csv("data/df_raw.csv")
+    df = pd.melt(df, id_vars=["url", "classification"], value_vars=["1A", "7", "7A"])
+    df = df.drop(["variable"], axis=1)
+    df = df[df["value"] != "undefined"]
 
+    print(1)
+    df["value"] = df["value"].apply(lambda x: " ".join([ent.text for ent in nlp(x) if not ent.ent_type_]))
+    print(2)
+    df["value"] = df["value"].apply(tokenize_and_stopwords)
+    print(3)
 
-# LogReg
-print("Logistic Regression")
-X = df.drop(['url', 'variable', 'classification'], axis=1)
-y = df['classification']
-x_train, x_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=2020)
+    df["benfords"] = df["value"].apply(benfords)
 
-# # transformer = ColumnTransformer(
-# #     [('vec', TfidfVectorizer(), 'value')],
-# #     remainder='passthrough'
-# # )
+    sia = SentimentIntensityAnalyzer()
+    df["polarity"] = df["value"].apply(lambda x: sia.polarity_scores(x))
+    print(4)
+    df.dropna(inplace=True)
+    df.to_csv("data/df_final.csv")
 
-# # pipe = Pipeline([('vect', transformer),
-# #                  ('model', LogisticRegression())])
+    # df = pd.read_csv("data/df_final.csv")
+    x_train, x_test, y_train, y_test = split_by_doc(df, test_size=0.3, target_name="classification", seed=2020)
 
-# # model = pipe.fit(x_train, y_train)
-# # prediction = model.predict(x_test)
-# # print("accuracy: {}%".format(round(accuracy_score(y_test, prediction)*100, 2)))
-# # print(confusion_matrix(y_test, prediction))
-# # print(classification_report(y_test, prediction))
+    x_train["benfords"] = pd.json_normalize(x_train["benfords"])
+    x_test["benfords"] = pd.json_normalize(x_test["benfords"])
+    x_train["polarity"] = pd.json_normalize(x_train["polarity"])
+    x_test["polarity"] = pd.json_normalize(x_test["polarity"])
 
-# # #SVC
-# # print("SVC")
-# # transformer = ColumnTransformer(
-# #     [('vec', TfidfVectorizer(ngram_range=(1,3)), 'value')],
-# #     remainder='passthrough'
-# # )
+    transformer = ColumnTransformer([("vec", TfidfVectorizer(ngram_range=(1, 3)), "value")], remainder="passthrough")
+    # # LogReg
+    # print("Logistic Regression")
+    # pipe = Pipeline([("vect", transformer), ("model", LogisticRegression())])
+    # test_model(pipe, x_train, x_test, y_train, y_test)
+    #
+    # # SVC
+    # print("SVC")
+    # pipe = Pipeline([("vect", transformer), ("model", SVC(kernel="poly"))])
+    # test_model(pipe, x_train, x_test, y_train, y_test)
+    #
+    # # Random forest classifier
+    # print("Random Forest Classifier")
+    # pipe = Pipeline([("vect", transformer), ("model", RandomForestClassifier())])
+    # test_model(pipe, x_train, x_test, y_train, y_test)
 
-# # pipe = Pipeline([('vect', transformer),
-# #                  ('model', SVC(kernel='poly'))])
+    # GradientBoostingClassifier
+    print("Gradient Boosting Classifier")
+    pipe = Pipeline([("vect", transformer), ("model", GradientBoostingClassifier())])
+    test_model(pipe, x_train, x_test, y_train, y_test)
 
-# # model = pipe.fit(x_train, y_train)
-# # prediction = model.predict(x_test)
-# # print("accuracy: {}%".format(round(accuracy_score(y_test, prediction)*100,2)))
-# # print(confusion_matrix(y_test, prediction))
-# # print(classification_report(y_test, prediction, digits=4))
-
-
-# # #random forest classifier
-# # print("Random Forest Classifier")
-# # transformer = ColumnTransformer(
-# #     [('vec', TfidfVectorizer(), 'value')],
-# #     remainder='passthrough'
-# # )
-
-# # pipe = Pipeline([('vect', transformer),
-# #                  ('model', RandomForestClassifier())])
-
-# # model = pipe.fit(x_train, y_train)
-# # prediction = model.predict(x_test)
-# # print("accuracy: {}%".format(round(accuracy_score(y_test, prediction)*100, 2)))
-# # print(confusion_matrix(y_test, prediction))
-# # print(classification_report(y_test, prediction, digits=4))
-
-
-#GradientBoostingClassifier
-print("Gradient Boosting Classifier")
-
-transformer = ColumnTransformer(
-    [('vec', TfidfVectorizer(), 'value')],
-    remainder='passthrough'
-)
-
-pipe = Pipeline([('vect', transformer),
-                 ('model', GradientBoostingClassifier())])
-
-model = pipe.fit(x_train, y_train)
-prediction = model.predict(x_test)
-print("accuracy: {}%".format(round(accuracy_score(y_test, prediction)*100, 2)))
-print(confusion_matrix(y_test, prediction))
-print(classification_report(y_test, prediction, digits=4))
-
-
-t1 = time.time()
-
-total = t1-t0
-print(total)
-
-# #Niave Baise
-# print("GaussianNB")
-# transformer = ColumnTransformer(
-#     [('vec', TfidfVectorizer(), 'value')],
-#     remainder='passthrough'
-# )
-
-# pipe = Pipeline([('vect', transformer),
-#                  ('dense', FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
-#                  ('model', GaussianNB())])
-
-# model = pipe.fit(x_train, y_train)
-# prediction = model.predict(x_test)
-# print("accuracy: {}%".format(round(accuracy_score(y_test, prediction)*100,2)))
-# print(confusion_matrix(y_test, prediction))
-# print(classification_report(y_test, prediction, digits=4))
-
-
-# #Multinomial niave baise
-
-# transformer = ColumnTransformer(
-#     [('vec', TfidfVectorizer(), 'value')],
-#     remainder='passthrough'
-# )
-
-# pipe = Pipeline([('vect', transformer),
-#                  ('dense', FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
-#                  ('scale', MinMaxScaler()),
-#                  ('model', MultinomialNB())])
-
-# model = pipe.fit(x_train, y_train)
-# prediction = model.predict(x_test)
-# print("accuracy: {}%".format(round(accuracy_score(y_test, prediction)*100,2)))
-# print(confusion_matrix(y_test, prediction))
-# print(classification_report(y_test, prediction, digits=4))
+    # # Naive bayes
+    # print("GaussianNB")
+    # pipe = Pipeline(
+    #     [
+    #         ("vect", transformer),
+    #         ("dense", FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
+    #         ("model", GaussianNB())
+    #     ]
+    # )
+    # test_model(pipe, x_train, x_test, y_train, y_test)
+    #
+    # # Multinomial naive bayes
+    # print("Multinomial NB")
+    # pipe = Pipeline(
+    #     [
+    #         ("vect", transformer),
+    #         ("dense", FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
+    #         ("scale", MinMaxScaler()),
+    #         ("model", MultinomialNB())
+    #     ]
+    # )
+    # test_model(pipe, x_train, x_test, y_train, y_test)
